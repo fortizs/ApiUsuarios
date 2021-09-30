900b81ed7817ba77aa9a3cab3dc7ac7bc426d51c
fler1987 <juanleder@hotmail.com>
2021-09-29 20:49:52 -0500

    crear logo

--- API.BussinessLogic/BLUsuarios.cs
+++ API.BussinessLogic/BLUsuarios.cs
@@ -21,5 +21,7 @@ namespace API.BussinessLogic
 
             return DAUsuarios.ListarUsuariosERP();
         }
+
+
     }
 }
---
+++ API.DataAccess/DALog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.DataAccess
+{
+    public class DALog
+    {
+
+        public BERetornoTran Guardar(BEBase pEntidad)
+        {
+            BERetornoTran BERetorno = new BERetornoTran();
+            SqlCommand cmd = ConexionCmd("gen.LogGuardar");
+            BE
+
+            cmd = LlenarEstructura(pEntidad, cmd, "I");
+            try
+            {
+
+
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                BERetorno.Retorno = Convert.ToString(cmd.Parameters["ReturnValue"].Value);
+                BERetorno.ErrorMensaje = Convert.ToString(cmd.Parameters["@ErrorMensaje"].Value);
+            }
+            catch (Exception ex)
+            {
+                BERetorno.ErrorMensaje = ex.ToString();
+            }
+            finally
+            {
+                if (cmd.Connection.State == ConnectionState.Open)
+                {
+                    cmd.Connection.Close();
+                }
+            }
+            return BERetorno;
+        }
+    }
+}
