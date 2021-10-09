80f887a75fced754ed5cda428dd690f9f4e9489e
fler1987 <juanleder@hotmail.com>
2021-10-09 13:55:14 -0500

    periodos

---
+++ API.Usuarios/Controllers/PeriodoController.cs
@@ -0,0 +1,33 @@
+﻿using API.Usuarios.Models;
+using API.Usuarios.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Usuarios.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeriodoController : ControllerBase
+    {        
+        private readonly IPeriodoService service;        
+        public PeriodoController(IPeriodoService services)
+        {
+            service = services;          
+        }
+
+        [HttpGet]
+        public ActionResult<List<Periodo>> GetAllPeriodo()
+        {
+            return service.AllPeriodo();
+        }
+
+        [HttpPost]
+        public ActionResult<Periodo> CreatePeriodo(Periodo periodo)
+        {
+            return service.SavePeriodo(periodo);
+        }
+    }
+}
--- API.Usuarios/Migrations/20211009184023_UpdateEmplid.Designer.cs
+++ API.Usuarios/Migrations/20211009184023_UpdateEmplid.Designer.cs
@@ -4,14 +4,16 @@ using API.Usuarios.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Usuarios.Migrations
 {
     [DbContext(typeof(MyDBContext))]
-    partial class MyDBContextModelSnapshot : ModelSnapshot
+    [Migration("20211009184023_UpdateEmplid")]
+    partial class UpdateEmplid
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
